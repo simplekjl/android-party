0509e3d003333630e8c3a6a6b3dd788192cdf002
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-13 21:38:55 +0200

    implementing session manager

--- app/app/src/main/java/com/simplekjl/servers/storage/SessionManagerImpl.kt
+++ app/app/src/main/java/com/simplekjl/servers/storage/SessionManagerImpl.kt
@@ -2,12 +2,13 @@ package com.simplekjl.servers.storage
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.simplekjl.domain.repository.SessionManager
 import com.simplekjl.servers.R
 
 /**
  * Session manager to save and fetch data from SharedPreferences
  */
-class SessionManagerImpl(context: Context) {
+class SessionManagerImpl(context: Context) : SessionManager {
     private var prefs: SharedPreferences =
         context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
 
@@ -15,26 +16,17 @@ class SessionManagerImpl(context: Context) {
         const val USER_TOKEN = "user_token"
     }
 
-    /**
-     * Function to save auth token
-     */
-    fun saveAuthToken(token: String) {
+    override fun saveAuthToken(token: String) {
         val editor = prefs.edit()
         editor.putString(USER_TOKEN, token)
         editor.apply()
     }
 
-    /**
-     * Function to fetch auth token
-     */
-    fun fetchAuthToken(): String? {
+    override fun fetchAuthToken(): String? {
         return prefs.getString(USER_TOKEN, null)
     }
 
-    /**
-     * Function to delete auth token
-     */
-    fun deleteAuthToken() {
+    override fun deleteAuthToken() {
         prefs.edit().clear().apply()
     }
 }
