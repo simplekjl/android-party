d140d276eb84888aa765d8212403ac2c954b2b2b
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-14 09:26:22 +0200

    Added domain contracts

--- app/app/src/main/java/com/simplekjl/servers/di/Module.kt
+++ app/app/src/main/java/com/simplekjl/servers/di/Module.kt
@@ -39,4 +39,5 @@ private fun createMainModule() = module {
             .build()
     }
     single<SessionManager> { SessionManagerImpl(androidContext()) }
+    factory { AuthInterceptor(get()) }
 }
