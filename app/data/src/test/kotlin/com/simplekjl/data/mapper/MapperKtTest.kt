d66f37fa1d134f9b43fc50855edc1da58db1f2b4
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-16 18:12:29 +0200

    update mapper test

---
+++ app/data/src/test/kotlin/com/simplekjl/data/mapper/MapperKtTest.kt
@@ -0,0 +1,30 @@
+package com.simplekjl.data.mapper
+
+import com.appmattus.kotlinfixture.decorator.nullability.NeverNullStrategy
+import com.appmattus.kotlinfixture.decorator.nullability.nullabilityStrategy
+import com.appmattus.kotlinfixture.kotlinFixture
+import com.simplekjl.data.model.ServerDetailsRaw
+import com.simplekjl.domain.model.ServerDetails
+import org.junit.Test
+import kotlin.test.assertTrue
+
+internal class MapperKtTest {
+
+    private val fixture = kotlinFixture {
+        nullabilityStrategy(NeverNullStrategy)
+    }
+
+    @Test
+    fun `when given an empty arrayListOf(ServerDetailsRaw) return emptyList`() {
+        val result = arrayListOf<ServerDetailsRaw>().toModel()
+        assert(result.isEmpty())
+    }
+
+    @Test
+    fun `when given an arrayListOf(ServerDetailsRaw) return listOf(ServerDetails)`() {
+        val testData: ArrayList<ServerDetailsRaw> = fixture()
+        val result = testData.toModel()
+        assert(result.isNotEmpty())
+        assert(result.size == testData.size)
+    }
+}
